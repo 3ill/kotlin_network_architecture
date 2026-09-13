885541ab12e52d94e081cb945c7cde4dd13d058e
3illBaby <thrillanon@outlook.com>
2026-09-13 16:53:55 +0100

    feat: define httpclient

--- src/main/kotlin/core/network/HttpClientFactory.kt
+++ src/main/kotlin/core/network/HttpClientFactory.kt
@@ -1,3 +1,23 @@
 package org.example.core.network
 
-object HttpClientFactory {}
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.okhttp.OkHttp
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+object HttpClientFactory {
+    fun create(): HttpClient {
+        return HttpClient(OkHttp) {
+            install(ContentNegotiation) {
+                json(
+                    Json {
+                        ignoreUnknownKeys = true
+                        isLenient = true
+                        explicitNulls = false
+                    }
+                )
+            }
+        }
+    }
+}
