9de5a03aa8aabfbf87d145bbc0f3e67707e84399
3illBaby <thrillanon@outlook.com>
2026-09-13 15:53:36 +0100

    chore: add ktor and coroutines deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,15 +1,27 @@
 plugins {
-    kotlin("jvm") version "2.4.10"
+    kotlin("jvm") version "2.4.20"
+    kotlin("plugin.serialization") version "2.4.20"
 }
 
 group = "org.example"
 version = "1.0-SNAPSHOT"
+
+val ktorVersion = "3.5.2"
+val coroutinesVersion = "1.11.0"
 
 repositories {
     mavenCentral()
 }
 
 dependencies {
+    implementation("io.ktor:ktor-client-core:${ktorVersion}")
+    implementation("io.ktor:ktor-client-okhttp:${ktorVersion}")
+    implementation("io.ktor:ktor-client-content-negotiation:${ktorVersion}")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:${ktorVersion}")
+    implementation("io.ktor:ktor-client-websockets:${ktorVersion}")
+    implementation("io.ktor:ktor-client-auth:${ktorVersion}")
+    implementation("io.ktor:ktor-client-logging:${ktorVersion}")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:${coroutinesVersion}")
     testImplementation(kotlin("test"))
 }
 
