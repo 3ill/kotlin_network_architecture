5c8c276cf4e22273be0b7c797365779d0e83eaf2
3illBaby <thrillanon@outlook.com>
2026-09-13 16:55:49 +0100

    feat: consume userApi

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,15 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+import org.example.core.network.HttpClientFactory
+import org.example.core.network.api.UserApi
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
-    }
+suspend fun main() {
+    val client = HttpClientFactory.create()
+    println("HTTP client created successfully")
+
+    val userApi = UserApi(client)
+    val response = userApi.getUsers()
+
+    println(response)
+    client.close()
 }
