c187cc74594ac189415051534df942060dd27730
3illBaby <thrillanon@outlook.com>
2026-09-13 16:54:27 +0100

    feat: fetch users without any serialization
    this returns our output as a string

--- src/main/kotlin/core/network/api/UserApi.kt
+++ src/main/kotlin/core/network/api/UserApi.kt
@@ -1,2 +1,12 @@
 package org.example.core.network.api
 
+import io.ktor.client.HttpClient
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+
+class UserApi(private val _client: HttpClient) {
+    suspend fun getUsers(): String {
+        return _client.get("https://jsonplaceholder.typicode.com/users").bodyAsText()
+
+    }
+}
