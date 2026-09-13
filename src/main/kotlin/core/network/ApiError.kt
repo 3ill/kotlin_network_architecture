c319e1bc755c0493bfc4026d28822635b19d0b48
3illBaby <thrillanon@outlook.com>
2026-09-13 19:56:35 +0100

    chore: defined api errors

--- src/main/kotlin/core/network/ApiError.kt
+++ src/main/kotlin/core/network/ApiError.kt
@@ -1,2 +1,14 @@
 package org.example.core.network
 
+sealed interface ApiError {
+    data class Http(
+        val statusCode: Int,
+        val responseBody: String
+    ) : ApiError
+
+    data class Network(val message: String?): ApiError
+
+    data class Serialization(val message: String?): ApiError
+
+    data class Unknown(val message: String?): ApiError
+}
