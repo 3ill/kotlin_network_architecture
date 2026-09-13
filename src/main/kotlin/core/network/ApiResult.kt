0a1a45bd3f3f070f30912c43192dac77af9b1e36
3illBaby <thrillanon@outlook.com>
2026-09-13 19:56:53 +0100

    chore: Defined api result

---
+++ src/main/kotlin/core/network/ApiResult.kt
@@ -0,0 +1,8 @@
+package org.example.core.network
+
+
+sealed interface ApiResult<out T> {
+    data class Success<T>(val data: T): ApiResult<T>
+
+    data class Failure(val error: ApiError): ApiResult<Nothing>
+}
