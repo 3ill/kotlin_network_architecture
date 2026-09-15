b162007a18c82f03beedfde8299c5177dd5f5123
3illBaby <thrillanon@outlook.com>
2026-09-15 14:19:13 +0100

    chore: register websocket

--- src/main/kotlin/core/network/HttpClientFactory.kt
+++ src/main/kotlin/core/network/HttpClientFactory.kt
@@ -5,6 +5,7 @@ import io.ktor.client.engine.okhttp.OkHttp
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import io.ktor.client.plugins.websocket.WebSockets
 
 object HttpClientFactory {
     fun create(): HttpClient {
@@ -17,6 +18,10 @@ object HttpClientFactory {
                         explicitNulls = false
                     }
                 )
+            }
+
+            install(WebSockets) {
+                pingIntervalMillis = 20_000
             }
         }
     }
