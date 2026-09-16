1f6d6c71b3a73d2a7e3c1aa3a79349f5a86dc984
3illBaby <thrillanon@outlook.com>
2026-09-16 16:46:29 +0100

    feat: configure retry policy

--- src/main/kotlin/core/network/HttpClientFactory.kt
+++ src/main/kotlin/core/network/HttpClientFactory.kt
@@ -1,21 +1,55 @@
 package org.example.core.network
 
-import io.ktor.client.HttpClient
-import io.ktor.client.call.body
-import io.ktor.client.engine.okhttp.OkHttp
-import io.ktor.client.plugins.auth.Auth
-import io.ktor.client.plugins.auth.providers.BearerTokens
-import io.ktor.client.plugins.auth.providers.bearer
-import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
-import io.ktor.serialization.kotlinx.json.json
+import io.ktor.client.*
+import io.ktor.client.call.*
+import io.ktor.client.engine.okhttp.*
+import io.ktor.client.plugins.*
+import io.ktor.client.plugins.auth.*
+import io.ktor.client.plugins.auth.providers.*
+import io.ktor.client.plugins.contentnegotiation.*
+import io.ktor.client.plugins.websocket.*
+import io.ktor.client.request.*
+import io.ktor.http.*
+import io.ktor.network.sockets.SocketTimeoutException
+import io.ktor.serialization.kotlinx.json.*
 import kotlinx.serialization.json.Json
-import io.ktor.client.plugins.websocket.WebSockets
-import io.ktor.client.request.post
-import io.ktor.client.request.setBody
-import io.ktor.http.ContentType
-import io.ktor.http.contentType
 import org.example.data.dtos.TokenResponseDto
 import org.example.repositories.auth.AuthRepository
+import io.ktor.client.network.sockets.ConnectTimeoutException
+import kotlinx.io.IOException
+import java.net.ConnectException
+import java.net.UnknownHostException
+
+
+private val retryableStatusCodes = setOf(
+    408, // Request Timeout
+    425, // Too Early
+    429, // Too Many Requests
+    500, // Internal Server Error
+    502, // Bad Gateway
+    503, // Service Unavailable
+    504  // Gateway Timeout
+)
+
+private fun isRetryableMethod(
+    method: HttpMethod
+): Boolean {
+    return method in setOf(
+        HttpMethod.Get,
+        HttpMethod.Head,
+        HttpMethod.Options
+    )
+}
+
+private fun isRetryableNetworkError(
+    cause: Throwable
+): Boolean {
+    return cause is ConnectException
+            || cause is SocketTimeoutException
+            || cause is UnknownHostException
+            || cause is HttpRequestTimeoutException
+            || cause is IOException
+}
 
 
 object HttpClientFactory {
@@ -52,7 +86,7 @@ object HttpClientFactory {
 
                     refreshTokens {
                         val refreshToken = oldTokens?.refreshToken ?: return@refreshTokens null
-                        val response = refreshClient.post (""){
+                        val response = refreshClient.post("") {
                             contentType(ContentType.Application.Json)
                             setBody(
                                 mapOf("refreshToken" to refreshToken)
@@ -76,6 +110,29 @@ object HttpClientFactory {
 
             install(WebSockets) {
                 pingIntervalMillis = 20_000
+                maxFrameSize = 1L * 1024 * 1024 // 1mb
+            }
+
+
+            install(HttpTimeout) {
+                requestTimeoutMillis = 10_000
+                connectTimeoutMillis = 20_000
+                socketTimeoutMillis = 30_000
+            }
+
+
+            install(HttpRequestRetry) {
+                retryOnServerErrors(maxRetries = 5)
+                retryIf { request, response ->
+                    isRetryableMethod(request.method) &&
+                            response.status.value in retryableStatusCodes
+                }
+
+                retryOnExceptionIf { request, cause ->
+                    isRetryableMethod(request.method) &&
+                            isRetryableNetworkError(cause)
+                }
+                exponentialDelay()
             }
         }
     }
