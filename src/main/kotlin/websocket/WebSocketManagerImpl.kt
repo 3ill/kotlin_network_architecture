4b8f0ed094f8c8b1d984be24b8f376ab414029aa
3illBaby <thrillanon@outlook.com>
2026-09-16 12:42:45 +0100

    chore: renamed

--- src/main/kotlin/websocket/WebSocketManagerImpl.kt
+++
@@ -1,9 +0,0 @@
-package org.example.websocket
-
-import io.ktor.client.HttpClient
-
-class WebSocketManagerImpl(private val client: HttpClient): WebSocketManager {
-    override suspend fun connect() {
-        TODO("Not yet implemented")
-    }
-}
