264f638050d7088bc261ed2c1ad26705bf6340e3
3illBaby <thrillanon@outlook.com>
2026-09-16 12:40:54 +0100

    config: Extend websocket config

--- src/main/kotlin/core/config/WebsocketConfig.kt
+++ src/main/kotlin/core/config/WebsocketConfig.kt
@@ -1,5 +1,9 @@
-package org.example.api
+package org.example.core.config
 
 object WebsocketConfig {
+    const val MAX_ATTEMPTS = 5
     const val BASE_URL = "wss://your-server.example.com/socket"
+    const val BASE_DELAY = 1_000L
+    const val MAX_DELAY = 30_000L
+    const val HEARTBEAT_DELAY = 20_000L
 }
